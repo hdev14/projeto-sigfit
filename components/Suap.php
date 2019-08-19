7137db62e9c170735e5a07434e061d510f397634
HermersonDev <ne.hermerson@gmail.com>
2019-08-19 11:01:25 -0300

    Adição do component para autenticação via SUAP.

---
+++ components/Suap.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace app\components;
+
+
+use yii\base\Component;
+use GuzzleHttp\Client;
+
+class Suap extends Component
+{
+
+    private $client;
+
+    # Para autenticar -> POST autenticacao/token/
+    # Para verificar a validade do token -> POST autenticacao/token/verify/
+
+    public function __construct($config = [])
+    {
+        $this->client = new Client([
+            'base_uri' => 'https://suap.ifrn.edu.br/api/v2/',
+            'timeout' => 3.0,
+        ]);
+        parent::__construct($config);
+    }
+
+    public function autenticar($matricula, $senha)
+    {
+
+       $resposta = $this->client->post('autenticacao/token/', [
+           'json' => [
+               'username' => $matricula,
+               'password' => $senha,
+           ]
+       ]);
+
+       if ($resposta->getStatusCode() == 200) {
+            $dados = json_decode($resposta->getBody(), true);
+            return $dados['token'];
+       }
+
+       return false;
+
+    }
+
+}
---
+++ tests/unit/components/SuapTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+use Codeception\Test\Unit;
+use \app\components\Suap;
+
+class SuapTest extends Unit
+{
+    /**
+     * @test
+     */
+    public function verifica_se_token_esta_sendo_retornado()
+    {
+        $this->assertEquals(true, false);
+    }
+
+}
