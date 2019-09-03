5dea2c24100ff98aaf72bcaf753ae69502013605
HermersonDev <ne.hermerson@gmail.com>
2019-09-03 10:59:13 -0300

    Continuação da implementação de teste para o modelo PessoaSearch.

--- tests/unit/models/PessoaSearchTest.php
+++ tests/unit/models/PessoaSearchTest.php
@@ -84,4 +84,143 @@ class PessoaSearchTest extends Unit
 
         $transaction->rollBack();
     }
+
+    /**
+     * @test
+     * @expectedException \Throwable
+     * @expectedException \Exception
+     */
+    public function verificar_retorno_do_metodo_searchAlunos() {
+
+        /* @var $transaction Transaction */
+        $transaction = $this->db->beginTransaction();
+
+        try {
+
+            $instrutor = new Pessoa();
+            $instrutor->matricula = '192837465';
+            $instrutor->nome =  'instrutor de teste';
+            $instrutor->email = 'instrutordeteste@email.com';
+            $instrutor->save();
+
+            $aluno = new Pessoa();
+            $aluno->matricula = '5647382910';
+            $aluno->nome =  'aluno de teste';
+            $aluno->email = 'alunodeteste@email.com';
+            $aluno->save();
+
+            $usuario_instrutor_aluno = new UsuarioInstrutor();
+            $usuario_instrutor_aluno->instrutor_id = $instrutor->id;
+            $usuario_instrutor_aluno->usuario_id = $aluno->id;
+            $usuario_instrutor_aluno->save();
+
+            $transaction->commit();
+
+        } catch (\Exception $e) {
+            $transaction->rollBack();
+            throw $e;
+        } catch (\Throwable $e) {
+            $transaction->rollBack();
+            throw $e;
+        }
+
+        $pessoa_search = new PessoaSearch();
+        $query = $pessoa_search->searchAlunos('192837465');
+        $this->assertEquals(ActiveQuery::className(), $query::className());
+
+        $transaction->rollBack();
+    }
+
+    /**
+     * @test
+     * @expectedException \Exception
+     * @expectedException \Throwable
+     */
+    public function verificar_retorno_do_metodo_searchServidores() {
+
+        /* @var $transaction Transaction */
+        $transaction = $this->db->beginTransaction();
+
+        try {
+
+            $instrutor = new Pessoa();
+            $instrutor->matricula = '192837465';
+            $instrutor->nome =  'instrutor de teste';
+            $instrutor->email = 'instrutordeteste@email.com';
+            $instrutor->save();
+
+            $servidor = new Pessoa();
+            $servidor->matricula = '0912873465';
+            $servidor->nome =  'servidor de teste';
+            $servidor->email = 'servidordeteste@email.com';
+            $servidor->servidor = true;
+            $servidor->save();
+
+            $usuario_instrutor_servidor = new UsuarioInstrutor();
+            $usuario_instrutor_servidor->instrutor_id = $instrutor->id;
+            $usuario_instrutor_servidor->usuario_id = $servidor->id;
+            $usuario_instrutor_servidor->save();
+
+            $transaction->commit();
+
+        } catch (\Exception $e) {
+            $transaction->rollBack();
+            throw $e;
+        } catch (\Throwable $e) {
+            $transaction->rollBack();
+            throw $e;
+        }
+
+        $pessoa_search = new PessoaSearch();
+        $query = $pessoa_search->searchServidores('192837465');
+        $this->assertEquals(ActiveQuery::className(), $query::className());
+
+        $transaction->rollBack();
+    }
+
+     /**
+     * @test
+     * @expectedException \Exception
+     * @expectedException \Throwable
+     */
+    public function verificar_retorno_do_metodo_searchInstrutores() {
+
+        $this->markTestIncomplete('Ainda será refeito ');
+
+        /* @var $transaction Transaction */
+        $transaction = $this->db->beginTransaction();
+
+        try {
+
+            $instrutor1 = new Pessoa();
+            $instrutor1->matricula = '192837465';
+            $instrutor1->nome =  'instrutor1 de teste';
+            $instrutor1->email = 'instrutor1deteste@email.com';
+            $instrutor1->save();
+
+            $instrutor2 = new Pessoa();
+            $instrutor2->matricula = '2938475610';
+            $instrutor2->nome =  'instrutor2 de teste';
+            $instrutor2->email = 'instrutor2deteste@email.com';
+            $instrutor2->save();
+
+            // FAZER A QUERY E DA TABELA AUTH E ASSINAR OS INSTRUTORES COM A
+            // ROLE INSTRUTOR.
+
+            $transaction->commit();
+
+        } catch (\Exception $e) {
+            $transaction->rollBack();
+            throw $e;
+        } catch (\Throwable $e) {
+            $transaction->rollBack();
+            throw $e;
+        }
+
+        $pessoa_search = new PessoaSearch();
+        $query = $pessoa_search->searchServidores('192837465');
+        $this->assertEquals(ActiveQuery::className(), $query::className());
+
+        $transaction->rollBack();
+    }
 }
