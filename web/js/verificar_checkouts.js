84d8400082ca74f83eedfebdf47e4ef343056c4d
HermersonDev <ne.hermerson@gmail.com>
2019-11-19 09:32:33 -0300

    Imp. método para veirifcar horario do checkouts.

--- web/js/verificar_checkouts.js
+++ web/js/verificar_checkouts.js
@@ -1,25 +1,42 @@
 const URI = 'http://localhost:8080/?r=pessoa/verificar-checkouts';
+const horario_de_final_de_treino = [
+  '07:53',
+  '08:56',
+  '09:31',
+  '10:53',
+  '13:53',
+  '14:53',
+  '15:53',
+  '16:53',
+  '17:53',
+  '18:53'
+];
 
-function verificarCheckouts(resolve, reject)
+function verificarCheckouts()
 {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', URI);
-    xhr.send(null);
+    if (verificarHorario()) {
+       let xhr = new XMLHttpRequest();
+        xhr.open('GET', URI);
+        xhr.send(null);
 
-    xhr.onreadystatechange = function() {
-        if (xhr.status === 200 && xhr.readyState === 4)
-            resolve(JSON.parse(xhr.responseText));
-        else if (xhr.status >= 400 && xhr.readyState === 4)
-            reject(xhr.status);
+        xhr.onreadystatechange = function() {
+            if (xhr.status === 200 && xhr.readyState === 4)
+                console.log(JSON.parse(xhr.responseText), "OBJETO");
+            else if (xhr.status >= 400 && xhr.readyState === 4)
+                console.error(xhr.status);
+        }
     }
+
+    setInterval(verificarCheckouts, 60000);
 }
 
-let promise_verificar_checkouts = new Promise(verificarCheckouts);
+function verificarHorario() {
+    let horario = (new Date()).toLocaleTimeString()
+        , hora_minuto = horario.slice(0, 5);
+    console.log(hora_minuto, "HORA");
+    return horario_de_final_de_treino.find(value => value === hora_minuto);
+}
 
-promise_verificar_checkouts
-    .then((response) => {
-        console.log(response);
-    })
-    .catch((statusCode) => {
-        console.error(`Error ${statusCode}`);
-    });
+verificarCheckouts();
+
+
